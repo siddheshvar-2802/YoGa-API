1023c85e5afb226388c20ed1d7c54a45d39612cb
siddheshvar-2802 <mrsiddheshvarpatil@gmail.com>
2024-09-14 21:52:34 +0530

    initial commit

---
+++ src/test/java/com/yoga/api/YoGaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoga.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoGaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
